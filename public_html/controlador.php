a5eaa97500d87128bf4668a56a39b25f3c99afe8
Pato <patricioperalta@gmail.com>
2025-02-24 23:19:20 -0500

    Login Funcional

---
+++ public_html/controlador.php
@@ -0,0 +1,18 @@
+<?php
+if (!empty($_POST["btningresar"])) {
+    if (empty($_POST["usuario"]) and empty($_POST["password"])) {
+        echo '<div style="color:red">LOS CAMPOS ESTAN VACIOS </div>';
+    }else{
+        $usuario=$_POST["usuario"];
+        $clave=$_POST["password"];
+
+        $sql=$conexion->query(" select * from usuario where u_usuario= '$usuario' and u_contrasena='$clave'");
+        if ($datos =$sql->fetch_object()) {
+            header("location:menu.php");
+        }else {
+            echo '<div style="color:red">Acceso denegado </div>';
+        }
+    }
+}
+
+?>
