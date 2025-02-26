cd9049a65747aaf90e6ffde1b686a41d494c7d3a
Pato <patricioperalta@gmail.com>
2025-02-25 19:37:22 -0500

    Login Funcional

--- public_html/menu.php
+++ public_html/menu.php
@@ -18,7 +18,7 @@
         <nav class="navbar navbar-expand-lg" style="background-color: #e2e2e2;">
             <div class="container px-lg-5">
                 <a class="navbar-brand" href="#!">
-                    <img src="imagenes\logo.png" alt="logo" width="150px">
+                    <img src="img\logo.png" alt="logo" width="150px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
