bc3ca5a462bf38818ff7a224767da8031fb74752
Pato <patricioperalta@gmail.com>
2025-03-01 00:14:07 -0500

    Creacion de buscador y poblacion de datos

--- public_html/menu.php
+++ public_html/menu.php
@@ -66,7 +66,7 @@ if ($varsesion==null || $varsesion='') {
                     <div class="col-lg-6 col-xxl-4 mb-5">
                         <div class="card bg-light border-0 h-100">
                             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-file-text"></i></div>
+                                <a href="tramites_varios.php" class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-file-text"></i></a>
                                 <h2 class="fs-4 fw-bold">Tramites Varios</h2>
                                 <p class="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
                             </div>
