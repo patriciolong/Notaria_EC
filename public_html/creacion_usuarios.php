92713907ad5ac5a23e813099b66a2d54f9ff3f33
Pato <patricioperalta090@gmail.com>
2025-06-06 13:42:05 -0500

    Nuevas funcionalidades

--- public_html/creacion_usuarios.php
+++ public_html/creacion_usuarios.php
@@ -25,7 +25,6 @@ if ($varsesion==null || $varsesion=='') {
     <link href="css/styles.css" rel="stylesheet" />
     <link href="css/register.css" rel="stylesheet" /> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
     <style>
         /* Importar fuente Lato para el Navbar */
         @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap');
@@ -150,11 +149,11 @@ if ($varsesion==null || $varsesion=='') {
     </nav>
 
     <div class="container">
-        <h2 class="form-title">Registrar Cliente</h2>
-        <form class="fs-form" action="register_controler.php" method="POST" id="clientesform">
+        <h2 class="form-title">Registrar Usuario</h2>
+        <form class="fs-form" action="register_usuarios_controler.php" method="POST" id="formusuarios">
         <?php
             include("conexionbd.php");
-            include("register_controler.php");
+            include("register_usuarios_controler.php");
             ?>
             <fieldset>
                 <div class="fs-field">
@@ -166,49 +165,17 @@ if ($varsesion==null || $varsesion=='') {
                     <input class="fs-input" name="apellido" />
                 </div>
                 <div class="fs-field">
-                    <label class="fs-label" for="">Edad</label>
-                    <input class="fs-input" name="edad" />
+                    <label class="fs-label" for="">Usuario</label>
+                    <input class="fs-input" name="usuario" />
                 </div>
                 <div class="fs-field">
-                    <label class="fs-label" for="">Identificacion</label>
-                    <input class="fs-input" name="identificacion" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Telefono</label>
-                    <input class="fs-input" name="telefono" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Direccion</label>
-                    <input class="fs-input" name="direccion" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Pais</label>
-                    <input class="fs-input"  name="pais" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Estado</label>
-                    <input class="fs-input"  name="estado" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Ciudad</label>
-                    <input class="fs-input"  name="ciudad" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Codigo Postal</label>
-                    <input class="fs-input"  name="postal" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">Email</label>
-                    <input class="fs-input"  name="email" />
-                </div>
-                <div class="fs-field">
-                    <label class="fs-label" for="">N° Departamento</label>
-                    <input class="fs-input"  name="departamento" />
+                    <label class="fs-label" for="">Contraseña</label>
+                    <input class="fs-input" name="pass" />
                 </div>
             </fieldset>
 
                 <div class="fs-button-group">
-    <input class="fs-button" type="submit" name="btn_registro" >
+    <input class="fs-button" type="submit" name="btn_registro_user" >
   </div>
 
         </form>
@@ -218,13 +185,13 @@ if ($varsesion==null || $varsesion=='') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/scripts.js"></script>
     <script>
-document.getElementById('clientesform').addEventListener('submit', function (e) {
+document.getElementById('formusuarios').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const formData = new FormData(this);
-    formData.append("btn_registro", "1");
+    formData.append("btn_registro_user", "1");
 
-    fetch('register_controler.php', {
+    fetch('register_usuarios_controler.php', {
         method: 'POST',
         body: formData
     })
--- public_html/menu.php
+++ public_html/menu.php
@@ -129,6 +129,30 @@ if ($varsesion==null || $varsesion=='') {
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <a href="creacion_usuarios.php" class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-file-text"></i></a>
+                                <h2 class="fs-4 fw-bold">Registrar Usuario</h2>
+
+                                <p class="mb-0"></p>
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <a href="listado_usuarios.php" class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-file-text"></i></a>
+                                <h2 class="fs-4 fw-bold">Listado de Usuarios</h2>
+
+                                <p class="mb-0"></p>
+
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
